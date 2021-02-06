ec353eef5d75fda29d13f7361abe6c3fb163b1a0
hwasiti <wasiti14@gmail.com>
2021-02-07 04:43:44 +0800

    Fixed unnecessary walking through all remained photos received from Flicker, even when we requested to download less n.

--- scraper.py
+++ scraper.py
@@ -14,13 +14,20 @@ KEY = credentials['KEY']
 SECRET = credentials['SECRET']
 
 
-def get_links(search_query, n=10, download=False):
+def download_photos(search_query, n=10, download=False):
     t = time.time()
     license = ()  # https://www.flickr.com/services/api/explore/?method=flickr.photos.licenses.getInfo
     fr = FlickrAPI(KEY, SECRET)
+    if n < 500:
+        per_page = n
+    else:
+        per_page = 500
+
+    # API library details:
+    # https://stuvel.eu/flickrapi-doc/7-util.html#walking-through-all-photos-in-a-set
     photos = fr.walk(text=search_query,
                      extras='url_o',
-                     per_page=500,  # 1-500
+                     per_page=per_page,  # 1-500
                      license=license,
                      sort='relevance')
 
@@ -34,7 +41,7 @@ def get_links(search_query, n=10, download=False):
     date_of_downloads = []
     time_of_downloads = []
     search_terms = []
-    urls = []
+    links = []
     for count, photo in enumerate(photos):
         if count < n:
             try:
@@ -53,12 +60,14 @@ def get_links(search_query, n=10, download=False):
                 date_of_downloads.append(now.strftime("%d/%m/%Y"))
                 time_of_downloads.append(now.strftime("%H:%M:%S"))
                 search_terms.append(search_query)
-                urls.append(link)
+                links.append(link)
                 print(f'{count + 1}/{n}: {link}')
             except Exception as e:
                 print(f'error in downloading item {count + 1}/{n}: {e}: {link}')
+        else:
+            break
 
-    data = list(zip(search_terms, date_of_downloads, time_of_downloads, filenames, urls))
+    data = list(zip(search_terms, date_of_downloads, time_of_downloads, filenames, links))
     df = pd.DataFrame(data, columns=['search term',
                                      'date of download',
                                      'time of download',
@@ -81,8 +90,8 @@ if __name__ == '__main__':
     assert KEY and SECRET, f'Get your Flickr API key and Secret. To apply visit {fr_key_help}. Save them in a file ' \
                            f'named: credentials.json as {"KEY":"YOUR_API_KEY", "SECRET":"YOUR_API_SECRET"}'
 
-    pd_data = get_links(search_query=args.search,  # search term
-                        n=args.n,  # max number of images
-                        download=args.download)  # download images
+    pd_data = download_photos(search_query=args.search,  # search term
+                              n=args.n,  # max number of images
+                              download=args.download)  # download images
 
     pd_data.to_csv(args.search + "_df.csv")
