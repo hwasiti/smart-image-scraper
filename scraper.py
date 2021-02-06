44dcd59e0be0ab64001dd6720e48f0c7f9d19957
hwasiti <wasiti14@gmail.com>
2021-02-07 05:52:50 +0800

    Added optional Geographical Bounding Box argument

--- scraper.py
+++ scraper.py
@@ -14,22 +14,26 @@ KEY = credentials['KEY']
 SECRET = credentials['SECRET']
 
 
-def download_photos(search_query, n=10, download=False):
+def download_photos(search_query, n=10, download=False, bbox = None):
     t = time.time()
-    license = ()  # https://www.flickr.com/services/api/explore/?method=flickr.photos.licenses.getInfo
     fr = FlickrAPI(KEY, SECRET)
     if n < 500:
         per_page = n
     else:
         per_page = 500
 
+    # bbox should be: minimum_longitude, minimum_latitude, maximum_longitude, maximum_latitude
+    if bbox is not None and len(bbox) == 4:
+        bbox = ','.join(bbox)
+
     # API library details:
     # https://stuvel.eu/flickrapi-doc/7-util.html#walking-through-all-photos-in-a-set
+    # http://www.flickr.com/services/api/flickr.photos.search.html
     photos = fr.walk(text=search_query,
                      extras='url_o',
                      per_page=per_page,  # 1-500
-                     license=license,
-                     sort='relevance')
+                     sort='relevance',
+                     bbox=bbox)
 
     if download:
         # image save folder
@@ -46,9 +50,13 @@ def download_photos(search_query, n=10, download=False):
         if count < n:
             try:
                 link = photo.get('url_o')  # photo with the original size
+
+                # http://www.flickr.com/services/api/flickr.photos.search.html
                 if link is None:
                     link = 'https://farm%s.staticflickr.com/%s/%s_%s_b.jpg' % \
                            (photo.get('farm'), photo.get('server'), photo.get('id'), photo.get('secret'))
+
+                metadata = photo.get('')
 
                 if download:
                     suffix = os.path.basename(link).split('.')[-1]
@@ -80,18 +88,35 @@ def download_photos(search_query, n=10, download=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--search', type=str, default='monkey cage', help='search term')
-    parser.add_argument('--n', type=int, default=10, help='number of images to download')
-    parser.add_argument('--download', type=bool, default=True, help='download images')
+    parser.add_argument('--search', '-s', dest='search', type=str, default='monkey cage', help='search term')
+    parser.add_argument('--number', '-n', dest='n', type=int, default=10, help='number of images to download')
+    parser.add_argument('--download', '-d', dest='download', type=bool, default=True, help='download images (True or False)')
+    parser.add_argument('--bbox', '-b', dest='bbox', required=False,
+                        help='Geographical bounding box to search in. Should be separated by spaces like: '
+                             'minimum_longitude minimum_latitude maximum_longitude maximum_latitude. '
+                             ' Longitude has a range of -180 to 180 , latitude of -90 to 90')
     args = parser.parse_args()
 
     # Check Flicker API key available
     fr_key_help = 'https://www.flickr.com/services/apps/create/apply'
     assert KEY and SECRET, f'Get your Flickr API key and Secret. To apply visit {fr_key_help}. Save them in a file ' \
                            f'named: credentials.json as {"KEY":"YOUR_API_KEY", "SECRET":"YOUR_API_SECRET"}'
+    try:
+        bbox = args.bbox.split(' ')
+    except Exception as e:
+        bbox = None
+        print('Geographical bounding box parsing error -> bbox will be ignored!')
+
+    if bbox and len(bbox) != 4:
+        bbox = None
+        print('Geographical bounding box parsing error -> bbox will be ignored!')
+
+    if bbox:
+        print('Searching within the Geographical bounding box: min_long, min_lat, max_long, max_lat', bbox)
 
     pd_data = download_photos(search_query=args.search,  # search term
                               n=args.n,  # max number of images
-                              download=args.download)  # download images
+                              download=args.download, # download images
+                              bbox=bbox) # search within certain geographical limit
 
     pd_data.to_csv(args.search + "_df.csv")
