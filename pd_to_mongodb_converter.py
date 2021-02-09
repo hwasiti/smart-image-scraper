89c55925c3dce1323ba643fadb232b448f9e345a
hwasiti <wasiti14@gmail.com>
2021-02-09 08:04:20 +0800

    Encryption tests

--- pd_to_mongodb_converter.py
+++ pd_to_mongodb_converter.py
@@ -1,6 +1,7 @@
 import os
 from pymongo import MongoClient
 import pandas as pd
+from cryptography.fernet import Fernet
 
 # Reading previously saved Pandas DataFrames
 fn = 'output' + os.sep + 'df_100_per_search_with_PREDS.json'
@@ -15,8 +16,33 @@ df_100_per_search_with_PREDS.reset_index(inplace=True)
 df_all.set_index("filename", inplace=True)
 df_all.reset_index(inplace=True)
 
-# TODO: Here or in prev Script: NEED to Encrypting sensitive data in df then deleting df1 and df2 from the storage (comment out the delete step)
+# Encrypt sensitive fields: 'owner name', 'path alias', 'owner', 'exif data'
+key = Fernet.generate_key()  # store in a secure location. You will needed when you decrypt the DB
+print("Key:", key.decode())
+with open('KEY_DB.txt', 'w') as file:
+    file.write(key.decode())
 
+# Read the key from the stored key file
+with open('KEY_DB.txt', 'r') as file:
+    key = file.read().encode()
+print("Key:", key.decode())
+
+def encrypt(message: bytes, key: bytes) -> bytes:
+    return Fernet(key).encrypt(message)
+
+
+def decrypt(token: bytes, key: bytes) -> bytes:
+    return Fernet(key).decrypt(token)
+
+
+message = 'John Doe'
+token = encrypt(message.encode(), key)
+print(token)
+
+message = decrypt(token, key).decode()
+print(message)
+
+pass
 # Insert a Pandas dataframe to MongoDB
 myclient = MongoClient("mongodb://localhost:27017/")
 mydb_100 = myclient["scraped_data_100"]
@@ -30,4 +56,3 @@ mycol_100.insert_many(mongo_data_100)
 
 mycol_all = mydb_all["scraped_data_all"]
 mycol_all.insert_many(mongo_data_all)
-
