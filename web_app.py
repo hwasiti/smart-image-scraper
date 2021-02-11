01767b18e5645d651744703e6ad2b6e561918650
hwasiti <wasiti14@gmail.com>
2021-02-12 05:33:21 +0800

    pagination of pictures grids
    Just assign a page number manually at this point at line 93

--- web_app.py
+++ web_app.py
@@ -72,7 +72,7 @@ def main():
 
     st.sidebar.title("Settings")
     st.sidebar.header("Grid size")
-    grid_size = st.sidebar.slider('', 1, 8, 2)
+    grid_size = st.sidebar.slider('', 1, 8, 8)
 
     columns = ['search term', 'date of download', 'time of download', 'filename', 'link',
                 'title', 'description', 'date taken', 'owner name', 'path alias', 'owner',
@@ -90,22 +90,24 @@ def main():
     df_filtered = df_decrypted.copy()
     # df_filtered = df_decrypted.iloc[:grid_size * grid_size, :].copy()
 
-    page = 2
-    n_images = grid_size * grid_size  # number of images in a page
-    if page * n_images <= len(df_filtered):
-        first_image_n = n_images * (page-1)
-        last_img_n = n_images * page
-        last_y = ceil(n_images/grid_size)
+    page = 4
+    n_grid_images = grid_size * grid_size  # number of images in a page
+    if page * n_grid_images <= len(df_filtered):
+        first_image_n = n_grid_images * (page - 1)
+        last_img_n = n_grid_images * page
+        last_y = ceil(n_grid_images / grid_size)
+        n_images_to_show = n_grid_images
     else:
-        first_image_n = n_images * (page - 1)
+        first_image_n = n_grid_images * (page - 1)
         last_img_n = len(df_filtered)
-        last_y = ceil(n_images/grid_size)
+        last_y = ceil((last_img_n - (page-1) * n_grid_images) / grid_size)
+        n_images_to_show = last_img_n - (page - 1) * n_grid_images
 
     for y in range(last_y):
         cols = st.beta_columns(grid_size)
-        last_x = n_images - (last_y - 1) * grid_size
+        last_x = n_images_to_show - (last_y - 1) * grid_size
         for x in range(last_x):
-            img_name = df_filtered.filename[(page-1) * n_images + y * grid_size + x]
+            img_name = df_filtered.filename[(page-1) * n_grid_images + y * grid_size + x]
             fn = "images_100" + os.sep + img_name
             image = Image.open(fn)
             if image.mode in ("RGBA", "P"):
